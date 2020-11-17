6053bdaeee832bc1e51af70f6480f5403afad6de
Maggie Yan <4everyan@gmail.com>
2020-11-16 22:37:16 -0500

    backEnd setup

---
+++ app/models/user.rb
@@ -0,0 +1,67 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  username        :string
+#  first_name      :string
+#  last_name       :string
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  age             :integer          not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+    validates :email, :username, :session_token, uniqueness: true, presence: true
+    validates :password_digest, presence: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
+
+    after_initialize :ensure_session_token, :set_username, :set_first_name
+    attr_reader :password
+    
+  def self.find_by_credentials(email, password)
+		user = User.find_by(email: email)
+		return nil unless user && user.is_password?(password)
+		user
+  end
+  
+
+  def password=(password)
+    @password = password    
+	self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+	bc_password = BCrypt::Password.new(self.password_digest)
+	bc_password.is_password?(password) 
+  end
+  
+  def reset_session_token!
+	self.session_token = self.class.generate_session_token
+    self.save! 
+	self.session_token
+  end
+	
+  
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+  def self.generate_session_token
+	SecureRandom::urlsafe_base64
+  end
+
+  def set_username
+    name = self.email.split("@")
+    self.username ||= name[1];
+  end
+
+  def set_first_name
+    name = self.email.split("@")
+    self.username ||= name[1];
+  end
+end
