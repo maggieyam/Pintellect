1a0c2d4f3234f86729383633434c16ef11a8516f
Maggie Yan <4everyan@gmail.com>
2020-11-23 10:00:02 -0500

    ready to merge

--- frontend/components/boards/index/board_index_item.jsx
+++ frontend/components/boards/index/board_index_item.jsx
@@ -7,15 +7,6 @@ const BoardIndexItem = ({board, openModal}) => {
     return (
       <div id="index-item">
         <h3 id="boards">{board.title}</h3>
-        {/* <button onClick={this.update()}></button> */}
-        {/* <FontAwesomeIcon
-          icon={faPen}
-          id="edit-board"
-          className="icon"
-            value={board.id}
-          size="lg"
-          onClick={() => openModal('update')}
-        /> */}
         <FontAwesomeIcon
           icon={faPen}
           id="edit-board"
@@ -23,11 +14,6 @@ const BoardIndexItem = ({board, openModal}) => {
           size="2x"
           onClick={() => openModal(modal)}
         />
-
-        {/* <Link to={`/${props.authorId}/${props.board.id}`}>{props.board.title}</Link> */}
-        {/* <button className="edit" onClick={redirect}>
-                <i className="edit-icon"></i>
-            </button> */}
       </div>
     );
   
