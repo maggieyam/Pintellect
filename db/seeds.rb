62b8559f59d70105d481f8b717d59a1387dba408
Maggie Yan <4everyan@gmail.com>
2020-11-29 13:53:47 -0500

    require uri

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # require './images.rb'
+require 'open-uri'
 ActiveRecord::Base.transaction do 
 
     User.destroy_all
