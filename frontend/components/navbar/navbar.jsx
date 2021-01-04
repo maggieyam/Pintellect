df4098011cc88ef0b979755bedbf87b5e13cf941
Maggie Yan <4everyan@gmail.com>
2021-01-04 18:43:34 -0500

    delete reset for search on navbar

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -4,45 +4,45 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faSearch,  faUserCog, faSignOutAlt, faStroopwafel} from '@fortawesome/free-solid-svg-icons';
 import {createIcon, createSpinIcon, createButtonLink} from '../../utils/graphics.util';
 // import PinIndexContainer from "../pins/pins_index/pins_index_container";
-import {filter} from '../search/search_pin'
+import SearchPin from '../search/search_pin'
 
 class Navbar extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      keywords: "",
-    }
-  }
-  componentDidMount() {
-    this.props.requestPins();
-  }
-  update() {
-    return e => {
-        e.preventDefault();
-        this.setState({keywords: e.currentTarget.value})
-    }
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     keywords: "",
+  //   }
+  // }
+  // componentDidMount() {
+  //   this.props.requestPins();
+  // }
+  // update() {
+  //   return e => {
+  //       e.preventDefault();
+  //       this.setState({keywords: e.currentTarget.value})
+  //   }
+  // }
 
-  onKeyEnter() {
-    return e => 
-      e.key === 'Enter' ? filter(this.props.pins, this.state.keywords) : null;
-  }
+  // onKeyEnter() {
+  //   return e => 
+  //     e.key === 'Enter' ? <SearchPin pins={this.props.pins} col={this.state.cols} keywords={this.state.keywords}/> : null;
+  // }
 
-  searchBar() {
-    return(
-      <>
-        <FontAwesomeIcon icon={faSearch} className="search-icon" size="lg"/>
-        <div onKeyPress={this.onKeyEnter()} id="search-container">
-          <input type="text" placeholder="Search"  value={this.state.keywords} onChange={this.update()} id="search"/>
-        </div>
-      </>
-    )
-  }
+  // searchBar() {
+  //   return(
+  //     <>
+  //       <FontAwesomeIcon icon={faSearch} className="search-icon" size="lg"/>
+  //       <div onKeyPress={this.onKeyEnter()} id="search-container">
+  //         <input type="text" placeholder="Search"  value={this.state.keywords} onChange={this.update()} id="search"/>
+  //       </div>
+  //     </>
+  //   )
+  // }
 
-  reset() {
-    filter(this.props.pins, "");
-    this.setState({keywords: ""});
-  }
+  // reset() {
+  //   filter(this.props.pins, "");
+  //   this.setState({keywords: ""});
+  // }
 
   render() {
     if (!this.props.currentUser) return null;
@@ -52,7 +52,7 @@ class Navbar extends React.Component{
         <div className="left-nav-icons">
           {createSpinIcon(faStroopwafel, "board-logo", "2x")}
            <Link to='/' id="home-link" >
-            <button className="board-nav-button" id="home-button" onClick={() => this.reset()}>
+            <button className="board-nav-button" id="home-button">
                 Home
             </button>
           </Link>      
